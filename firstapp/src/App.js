ab611a9abacc9e5d7d531b08b238d4945e858a48
Sarita Maurya <sm7767119@gmail.com>
2022-04-13 22:03:43 +0530

    new

---
+++ firstapp/src/App.js
@@ -0,0 +1,48 @@
+import './App.css';
+// import About from './components/About';
+import Navbar from './components/Navbar';
+ import TextForm from './components/TextForm';
+ import React, {useState} from 'react'
+ import Alert from './components/Alert';
+
+function App() {
+  const [mode, setMode] = useState('light');//whether dark mode is anabled or not
+  const [alert, setAlert] = useState(null);
+
+  const showAlert=(message,type)=>{
+    setAlert({
+      msg:message,
+      type:type
+    })
+    setTimeout(()=>{
+      setAlert(null);
+    },2000);
+  }
+
+  const toggleMode=()=>{
+    if(mode==='light'){
+      setMode('dark');
+      document.body.style.backgroundColor='#042743';
+      showAlert("Dark mode has been enabled","success");
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor='white';
+      showAlert("Light mode has been enabled","success");
+    }
+  }
+  return (
+  <>
+{/* <Navbar title="TextUtils" AboutText="About TextUtils"/> */}
+{/*<Navbar/>*/}
+<Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+<Alert alert={alert}/>
+<div className="container my-3">
+<TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode} />
+{/* <About/> */}
+</div>
+  </>
+  );
+}
+
+export default App;
